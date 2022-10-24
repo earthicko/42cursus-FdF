1d9157dbf481f26c9cb003c8fe94df28447708df
earthicko <earthicko@gmail.com>
2022-10-24 20:30:21 +0900

    libft에 atoi_if_valid, split_by_chars 추가

--- libft/ft_free_strarr.c
+++ libft/ft_free_strarr.c
@@ -1,39 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*   ft_free_strarr.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: donghyle <donghyle@student.42seoul.>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/07/08 13:43:21 by donghyle          #+#    #+#             */
-/*   Updated: 2022/07/08 13:43:22 by donghyle         ###   ########.fr       */
+/*   Created: 2022/07/10 13:54:11 by donghyle          #+#    #+#             */
+/*   Updated: 2022/07/10 13:54:12 by donghyle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_atoi(const char *str)
+void	*ft_free_strarr(char **strarr)
 {
 	char	*cursor;
-	long	output;
-	long	sign;
 
-	output = 0;
-	cursor = (char *) str;
-	while (*cursor != '\0' && ft_isspace(*cursor))
-		cursor++;
-	sign = 1;
-	if (*cursor == '+' || *cursor == '-')
-	{
-		if (*cursor == '-')
-			sign = -1;
-		cursor++;
-	}
-	while (*cursor != '\0' && ft_isdigit(*cursor))
-	{
-		output = output * 10 + ((long)(*cursor - '0'));
-		cursor++;
-	}
-	output *= sign;
-	return ((int) output);
+	cursor = *strarr;
+	while (cursor)
+		free(cursor++);
+	free(strarr);
+	return (NULL);
 }
